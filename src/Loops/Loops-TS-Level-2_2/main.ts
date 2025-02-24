8fc8d7fefe078c91f1f38bda94cd0024ea3a8081
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2025-02-24 21:13:27 +0100

    updates

--- src/Loops/Loops-TS-Level-2_2/main.ts
+++ src/Loops/Loops-TS-Level-2_2/main.ts
@@ -1,14 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const input = document.getElementById("numInput") as HTMLInputElement
-    const button = document.getElementById("generateButton") as HTMLButtonElement
-    const output = document.getElementById("output") as HTMLParagraphElement
+  const input = document.getElementById("numInput") as HTMLInputElement;
+  const button = document.getElementById("generateButton") as HTMLButtonElement;
+  const output = document.getElementById("output") as HTMLParagraphElement;
 
-    if (!input || !button || !output) return;
+  if (!input || !button || !output) return;
 
-    button.addEventListener("click", () => {
-        const count = Number(input.value);
-        
-        
-        output.textContent = `L${"o".repeat(count)}p`;
-    });
+  button.addEventListener("click", () => {
+    const count = Number(input.value);
+
+    output.textContent = `L${"o".repeat(count)}p`;
+  });
 });
+
+//# die zweite Lösung
+
+// function generataLoop() {
+//   const num = parseInt(
+//     (document.getElementById("numInput") as HTMLInputElement).value, 10  
+//   );
+//   let text = "L";
+//   for(let i = 0;i<num;i++)
+//   {
+//     text += "o";
+//   }
+//   text += "p";
+
+//   document.getElementById("output")!.textContent = text;
+// }
