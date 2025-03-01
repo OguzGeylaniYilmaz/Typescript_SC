be1171706fccc1102691d4e7f83cb6c8b39ebaf5
Oğuz Geylani Yılmaz <oguzgeylaniyilmaz@gmail.com>
2025-03-01 14:54:01 +0100

    updates on DOM

--- src/DOM/Dom-Elements-TS-Level-1_2/main.ts
+++ src/DOM/Dom-Elements-TS-Level-1_2/main.ts
@@ -1,15 +1,12 @@
-
-
 const classElements = document.querySelectorAll(".example");
 
-
 if (classElements.length > 0) {
-    const lastElement = classElements[classElements.length - 1] as HTMLElement;  
-    lastElement.addEventListener("click",()=>
-    {
-        classElements.forEach((element)=>{
-           ( element as HTMLElement).style.backgroundColor = "black";
-           ( element as HTMLElement).style.color = "white";
-        })
+  const lastElement = classElements[classElements.length - 1] as HTMLElement;
+
+  lastElement.addEventListener("click", () => {
+    classElements.forEach((element) => {
+      (element as HTMLElement).style.backgroundColor = "black";
+      (element as HTMLElement).style.color = "white";
     });
-  }
+  });
+}
--- src/DOM/Dom-Elements-TS-Level-2_7/main.ts
+++ src/DOM/Dom-Elements-TS-Level-2_7/main.ts
@@ -1,2 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector("button");
+    const container = document.querySelector(".umwickeln");
+    let counter = 0;
 
+    button?.addEventListener("click", () => {
+        if (!container) return;
 
+        const box = document.createElement("div");
+        box.textContent = counter.toString();
+        box.classList.add("rechteck");
+
+        if (counter % 10 === 0) {
+            box.classList.add("weiss");
+        }
+
+        container.appendChild(box);
+        counter++;
+    });
+});
+      
+   
